b885f72e87728fa3b3616f4d6512e0c4af7287ed
Ajayi Temitope <topsite_edu@yahoo.com>
2025-12-06 15:11:27 +0200

    Add files via upload

--- new-year.js
+++ new-year.js
@@ -1,5 +1,16 @@
-const monthNames = ["January", "February", "March", "April", "May", "June",
-"July", "August", "September", "October", "November", "December"
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 const dat = new Date();
 tday = dat.getDate();
@@ -10,51 +21,47 @@ var this_year = new Date().getFullYear();
 document.getElementById("nyear").innerHTML = this_year;
 
 function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
 function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    // add a zero in front of numbers<10
-    m = checkTime(m);
-    s = checkTime(s);
-    if(h==12){
-        var a = "PM";
-    }
-    else if(h==0){
-        h = 12;
-        var a = "AM";
-    }
-    else if(h>12){
-        h = h - 12;
-        var a = "PM";
-    }
-    else var a = "AM";
-    document.getElementById('time').innerHTML = h + ":" + m + ":" + s + " " + a;
-    t = setTimeout(function() {
-        startTime()
-    }, 500);
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  s = checkTime(s);
+  if (h == 12) {
+    var a = "PM";
+  } else if (h == 0) {
+    h = 12;
+    var a = "AM";
+  } else if (h > 12) {
+    h = h - 12;
+    var a = "PM";
+  } else var a = "AM";
+  document.getElementById("time").innerHTML = h + ":" + m + ":" + s + " " + a;
+  t = setTimeout(function () {
+    startTime();
+  }, 500);
 }
 
 startTime();
 
-setTimeout(function ()
-{
-    if (self.name != '_refreshed_'){
-    self.name = '_refreshed_';
+setTimeout(function () {
+  if (self.name != "_refreshed_") {
+    self.name = "_refreshed_";
     self.location.reload(true);
-    } else {
-        self.name = ''; 
-    }
+  } else {
+    self.name = "";
+  }
 }, 1000);
 
-function getpos(n){
-    var s = ['th', 'st', 'nd', 'rd'];
-    var d = (n|0)%100;
-    return d > 3 && d < 21 ? 'th' : s[d%10] || 'th';
+function getpos(n) {
+  var s = ["th", "st", "nd", "rd"];
+  var d = (n | 0) % 100;
+  return d > 3 && d < 21 ? "th" : s[d % 10] || "th";
 }
